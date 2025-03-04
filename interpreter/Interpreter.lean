1f706792f621587985b83c892af98597b24d3659
Yonatan Reicher <yony252525@gmail.com>
2025-03-04 12:40:37 +0200

    Add parse module

--- interpreter/Interpreter.lean
+++ interpreter/Interpreter.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `InterpreterTest` library.
 -- Import modules here that should be built as part of the library.
 import Interpreter.Basic
+import Interpreter.Parse
