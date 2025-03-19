753a05147a0d82828e4d2f93a730157cdc19da1d
Yonatan Reicher <yony252525@gmail.com>
2025-03-19 18:35:01 +0200

    Updates

--- sandbox/Main.lean
+++ sandbox/Main.lean
@@ -2,7 +2,7 @@ import Mathlib.Tactic
 
 variable {n m : ℕ}
 variable {X Y : Finset ℕ}
-variable {data : X -> Y -> ℤ}
+variable {data : X -> Y -> ℕ}
 
 theorem test_theorem_1
 -- PROBLEM
@@ -21,16 +21,66 @@ theorem test_theorem_1
 
 theorem test_theorem_2
 -- This proposition is taken directly from the domino proof.
-: (∑ x ∈ X, ∑ y ∈ Y, y % 2) % 2 
-  = (∑ x ∈ X, (∑ y ∈ Y, y) % 2) % 2 := by
+: (∑ _ ∈ X, ∑ y ∈ Y, y % 2) % 2
+  = (∑ _ ∈ X, (∑ y ∈ Y, y) % 2) % 2 := by
   -- `apply?` takes so long...
   -- `suffices :` is different from `suffices`! apparantly..
   suffices (X.card * ∑ y ∈ Y, y % 2) % 2 = (X.card * (∑ y ∈ Y, y) % 2) % 2 by
     simpa
   suffices (∑ y ∈ Y, y % 2) % 2 = (∑ y ∈ Y, y) % 2 by
+    -- These lemmas have had to be found manually by opening the docs..
+    -- Every lemma must be looked for in three places: Init, Std, and MathLib.
+    -- - then, you must look at Basic/Lemmas/Whatever that namespace divides
+    -- definitions into.
     rw [Nat.mod_mod]
     rw [Nat.mul_mod]
     rw [this]
-    simp_all
-  exact Eq.symm (Finset.sum_nat_mod Y _ _)
+    rw [Nat.mul_mod_mod, Nat.mod_mul_mod]
+  -- The following 3 lines of code all solve the current goal:
+  -- 1. exact Eq.symm (Finset.sum_nat_mod Y _ _)
+  -- 2. simp [Finset.sum_nat_mod]
+  -- 3. rw [<-Finset.sum_nat_mod]
+  -- I think the rewrite is the most intuitive for me.
+  rw [<-Finset.sum_nat_mod]
 
+abbrev Finset.a (x: Finset α) := Finset.attach x
+theorem test_theorem_3
+-- This proposition is taken directly from the domino proof.
+: (∑ x ∈ X.a, ∑ y ∈ Y.a, data x y % 2) % 2
+  = (∑ x ∈ X.a, (∑ y ∈ Y.a, data x y % 2) % 2) % 2 := by
+  -- `apply?` takes so long...
+  -- `suffices :` is different from `suffices`! apparantly..
+  have : (∑ x ∈ X.a, ∑ y ∈ Y.a, Fin.ofNat' 2 (data x y))
+     = Fin.ofNat' 2 (∑ x ∈ X.a, ∑ y ∈ Y.a, data x y) := by
+    apply?
+
+
+
+  suffices ∀ x, (∑ y ∈ Y.a, data x y % 2) % 2 = (∑ y ∈ Y.a, data x y) % 2 by
+    -- These lemmas have had to be found manually by opening the docs..
+    -- Every lemma must be looked for in three places: Init, Std, and MathLib.
+    -- - then, you must look at Basic/Lemmas/Whatever that namespace divides
+    -- definitions into.
+    rw [Nat.mod_mod]
+    rw [Nat.mul_mod]
+    rw [this]
+    rw [Nat.mul_mod_mod, Nat.mod_mul_mod]
+  -- The following 3 lines of code all solve the current goal:
+  -- 1. exact Eq.symm (Finset.sum_nat_mod Y _ _)
+  -- 2. simp [Finset.sum_nat_mod]
+  -- 3. rw [<-Finset.sum_nat_mod]
+  -- I think the rewrite is the most intuitive for me.
+  rw [<-Finset.sum_nat_mod]
+
+-- PROBLEM: Monad notation acting weird with explicit name in `map`.
+def inc : StateM Nat Nat := do
+  let x ← get
+  set (x + 1)
+  return x
+
+example : (inc.map fun x => x % 2) = ((fun x => x % 2) <$> inc) := by
+  -- simp should be able to solve this, but it can't. This leads to various
+  -- problems, when wanting to use StateT.map with an explicit name like that.
+  -- it seems that it was not intended to be called by name like that, but that
+  -- is not something I have seen documented.
+  rfl
