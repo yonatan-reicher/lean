d423583a80c1046e00a4277b742ad76599a7090f
Yonatan Reicher <yony252525@gmail.com>
2025-04-15 20:02:31 +0300

    Update Basic.lean

--- Basic.lean
+++ Basic.lean
@@ -2,13 +2,49 @@ import Mathlib.Tactic
 
 variable {n m : ℕ}
 
+
+variable {X Y : Finset ℕ}
+variable {data : X -> Y -> ℤ}
+
+theorem test_theorem_1
+-- PROBLEM
+-- Here we want to use a sum, but we defined `data` to take elements only of X.
+-- So we need to turn our `x` into `{ x : ℕ // x ∈ X }` but we are lacking a
+-- proof for `x ∈ X`!
+-- : (∑ x ∈ X, ∑ y ∈ Y, data ⟨x, _⟩ y) = (∑ y ∈ Y, ∑ x ∈ X, data x y) := by
+-- The missing piece was `Finset.attach`!
+: (∑ x ∈ X.attach, ∑ y ∈ Y.attach, data x y)
+  = (∑ y ∈ Y.attach, ∑ x ∈ X.attach, data x y) := by
+  -- The proof was solved with a simple call to `apply?`
+  -- (exact Finset.sum_comm)
+  unfold Finset.sum
+  exact Multiset.sum_map_sum_map X.attach.val Y.attach.val
+  -- unfold Multiset.map
+
+theorem test_theorem_2
+-- This proposition is taken directly from the domino proof.
+: (∑ x ∈ X, ∑ y ∈ Y, y % 2) % 2 
+  = (∑ x ∈ X, (∑ y ∈ Y, y) % 2) % 2 := by
+  -- `apply?` takes so long...
+  -- `suffices :` is different from `suffices`! apparantly..
+  suffices (X.card * ∑ y ∈ Y, y % 2) % 2 = (X.card * (∑ y ∈ Y, y) % 2) % 2 by
+    simpa
+  suffices (∑ y ∈ Y, y % 2) % 2 = (∑ y ∈ Y, y) % 2 by
+    rw [Nat.mod_mod]
+    rw [Nat.mul_mod]
+    rw [this]
+    simp_all
+  exact Eq.symm (Finset.sum_nat_mod Y _ _)
+
+
+
 -- Definition of domino tiling
 
 @[ext]
 structure Point where
   x : ℕ
   y : ℕ
-deriving Repr
+deriving Repr, DecidableEq
 
 def point (pt : ℕ × ℕ) : Point :=
   ⟨pt.1, pt.2⟩
@@ -33,9 +69,13 @@ structure DominoTiling (n m : ℕ) where
 
 -- lemmas about cardinalities
 
+-- I am marking lemmas and theorems that could be @[simp] with `-- SIMP`
+
+-- SIMP
 lemma card_domino (domino : Domino) : (cells domino).card = 2 := by
   cases domino <;> trivial
 
+-- SIMP
 lemma card_board : (board n m).card = n * m := by
   unfold board
   rw [Finset.card_map]
@@ -43,10 +83,11 @@ lemma card_board : (board n m).card = n * m := by
   nth_rewrite 2 [←Finset.card_range m]
   rw [←Finset.card_product]
 
+-- SIMP
 lemma card_tiling (tiling : DominoTiling n m) :
   2 * tiling.tiles.card = n * m := by
   have := tiling.tiles.card_disjiUnion (cells ·) tiling.disjoint
-  simp [tiling.union, card_board, card_domino] at this
+  simp only [tiling.union, card_board, card_domino, Finset.sum_const, smul_eq_mul] at this
   omega
 
 -- classification of dominos
