64db953db0b584a62782482518efe24f97de5778
Yonatan Reicher <yony252525@gmail.com>
2025-05-20 16:45:11 +0300

    Add a test to make an automatic cast tactic

--- sandbox/Meta.lean
+++ sandbox/Meta.lean
@@ -26,3 +26,28 @@ macro_rules
 
 
 #reduce lambda ("x" => "y" => "x" "y" "y")
+
+
+-- cast is already taken
+def cst {α} (x : α) (h : α = β := by congr 1 <;> solve | simp_all | omega) : β := cast h x
+
+
+inductive Vec (a : Type) : Nat -> Type
+  | nil : Vec a 0
+  | cons : a -> Vec a n -> Vec a (n + 1)
+
+-- This type checks without cst...
+def vappend {a n m} (x: Vec a n) (y: Vec a m) : Vec a (m + n) :=
+  match x with
+  | .nil => y
+  | .cons h t => .cons h (vappend t y)
+
+-- But this doesn't! (Swapped n and m around)
+def vappend' {a n m} (x: Vec a n) (y: Vec a m) : Vec a (n + m) :=
+  match x with
+  | .nil => cst y
+  | .cons h t =>
+    let x := Vec.cons h (vappend t y)
+    have : n + 1 + m = m + n + 1 := by omega
+    have : Vec a (n + 1 + m) = Vec a (m + n + 1) := by simp_all
+    cst x
